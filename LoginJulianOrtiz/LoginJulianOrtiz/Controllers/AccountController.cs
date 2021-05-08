be65b79c2eb880ffcdeefe6e9057b4d86a5d88fb
JulianOrtizVillar <69093683+JulianOrtizVillar@users.noreply.github.com>
2021-05-08 00:05:15 -0500

    Login con c# y sql server

---
+++ LoginJulianOrtiz/LoginJulianOrtiz/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginJulianOrtiz.Models;
+using System.Data.SqlClient;
+
+
+namespace LoginJulianOrtiz.Controllers
+{
+    
+    public class AccountController : Controller
+    {
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        // GET: Account
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionString() 
+        {
+            con.ConnectionString = "data source=JULIANORTIZ; database=LogginJulian; integrated security =SSPI;";
+        }
+
+        public ActionResult Verify(Account acc) 
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from tbl_login where username='"+acc.Name+"' and password='"+acc.Password+"'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View();
+            }
+
+            else
+            {
+                con.Close();
+                return View();
+            }
+            
+            
+        }
+    }
+}
---
+++ LoginJulianOrtiz/LoginJulianOrtiz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoginJulianOrtiz.MvcApplication" Language="C#" %>
